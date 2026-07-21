e98e74491c1fb15eb933361d63ad6c2d7c9ebbb3
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 11:23:41 +0700

    menambahkan do setelah while

--- loopingdowhile.cpp
+++ loopingdowhile.cpp
@@ -12,4 +12,9 @@ int main () {
     cout << "PERULANGAN DO..WHILE" << endl;
     //nilai awal x = 1
     x = 1 + rand() % 10;
+
+    do {
+        cout << "TI, Bilangan acak = " << x << endl;
+        x = rand() % 10; //pengubah nilai x
+    } while (x <= 5); //syarat
 }
