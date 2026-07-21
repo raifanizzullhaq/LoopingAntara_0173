6f00e518c412cbf64c51b60c297b4ef220705e96
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 11:20:23 +0700

    menambahkan cout do while

--- loopingdowhile.cpp
+++ loopingdowhile.cpp
@@ -7,4 +7,9 @@ int main () {
     int x;
 
     srand(time(0));
+
+    //perulangan dengan do..while (minimal 1x dikerjakan)
+    cout << "PERULANGAN DO..WHILE" << endl;
+    //nilai awal x = 1
+    x = 1 + rand() % 10;
 }
