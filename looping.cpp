9da319eba68c9c13d60ccb58e724bb1a438541ce
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 10:53:01 +0700

    perulangan naik turun

---
+++ looping.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //perulangan dengan for
+    cout << "PERULANGAN FOR" << endl;
+
+    //perulangan dengan for naik
+    for (int i = 0; i < 4; i++) {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+    //perulangan dengan for turun
+    for (int i = 6; i > 0; i--) {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+
+    }
+    cout << endl;
+
+    return 0;
+}
