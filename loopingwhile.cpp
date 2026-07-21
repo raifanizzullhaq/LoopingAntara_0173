9bc0de9eae65bf67d222b178569eb822e10bb4db
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 11:13:54 +0700

    looping while

--- loopingwhile.cpp
+++ loopingwhile.cpp
@@ -3,4 +3,23 @@
 #include <ctime>
 using namespace std;
 
-int main()
+int main() {
+    int x;
+
+    srand(time(0));
+
+    //perulangan dengan while
+    cout << "PERULANGAN WHILE" << endl;
+    //Nilai awal x = 1
+    x = 1 + rand () % 10;
+
+    while (x <= 5) {
+        cout << "TI, Bilangan acak = " << x << endl;
+        x = rand() % 10; //pengubah dari nilai x
+    }
+
+    cout << "Bilangan acak while yang terakhir = " << x << endl;
+    cout << endl;
+
+    return 0;
+}
